6b5beb0136051deab8755c9940ce9f1590e493b1
abdelmalek.saksioui <abdelmalek.saksioui@cgm.com>
2021-12-02 09:27:05 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import foods from "./foods.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App">
+        <h1>Food List</h1>
+        {foods.map((food, index) => {
+            return <div key={index} className="food-wrapper">
+                <p> {food.name} </p>
+                <img src={food.image} style={{height: '5rem'}}/>
+            </div>
+        })}
+    </div>;
 }
 
 export default App;
